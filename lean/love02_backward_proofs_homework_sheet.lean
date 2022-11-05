3c56d3450bec46ba1c45b00955a4e5c67aad02f9
Simon <simon.lemal@pm.me>
2022-11-05 19:10:22 +0100

    Solved HW 2

--- lean/love02_backward_proofs_homework_sheet.lean
+++ lean/love02_backward_proofs_homework_sheet.lean
@@ -24,25 +24,52 @@ Section 2.3 in the Hitchhiker's Guide. -/
 
 lemma B (a b c : Prop) :
   (a → b) → (c → a) → c → b :=
-sorry
+begin
+  intros hab hca c,
+  apply hab,
+  exact hca c,
+end
 
 lemma S (a b c : Prop) :
   (a → b → c) → (a → b) → a → c :=
-sorry
+begin
+  intros habc hab ha,
+  apply habc,
+  exact ha,
+  exact hab ha,
+end
 
 lemma more_nonsense (a b c : Prop) :
   (c → (a → b) → a) → c → b → a :=
-sorry
+begin
+  intros hcaba hc hb,
+  apply hcaba,
+  exact hc,
+  intro ha,
+  exact hb,
+end
 
 lemma even_more_nonsense (a b c : Prop) :
   (a → a → b) → (b → c) → a → b → c :=
-sorry
+begin
+  intros haab hbc ha hb,
+  exact hbc hb,
+end
 
 /-! 1.2 (1 point). Prove the following lemma using basic tactics. -/
 
 lemma weak_peirce (a b : Prop) :
   ((((a → b) → a) → a) → b) → b :=
-sorry
+begin
+  intro f,
+  apply f,
+  intro g,
+  apply g,
+  intro a,
+  apply f,
+  intro h,
+  exact a,
+end
 
 
 /-! ## Question 2 (5 points): Logical Connectives
@@ -60,7 +87,15 @@ Hints:
 
 lemma about_implication (a b : Prop) :
   ¬ a ∨ b → a → b :=
-sorry
+begin
+  intros hab ha,
+  apply or.elim hab,
+  { intro hna,
+    apply false.elim,
+    exact hna ha, },
+  { intro hb,
+  exact hb, },
+end
 
 /-! 2.2 (2 points). Prove the missing link in our chain of classical axiom
 implications.
@@ -79,7 +114,19 @@ Hints:
 
 lemma em_of_dn :
   double_negation → excluded_middle :=
-sorry
+begin
+  rw double_negation,
+  rw excluded_middle,
+  intros hdn a,
+  apply hdn,
+  intro hnem,
+  apply hnem,
+  apply or.intro_right,
+  intro ha,
+  apply hnem,
+  apply or.intro_left,
+  exact ha,
+end
 
 /-! 2.3 (2 points). We have proved three of the six possible implications
 between `excluded_middle`, `peirce`, and `double_negation`. State and prove the
@@ -89,7 +136,29 @@ three missing implications, exploiting the three theorems we already have. -/
 #check dn_of_peirce
 #check em_of_dn
 
--- enter your solution here
+lemma em_of_pierce :
+  peirce → excluded_middle :=
+begin
+  intro hp,
+  apply em_of_dn,
+  exact dn_of_peirce hp,
+end
+
+lemma dn_of_em :
+  excluded_middle → double_negation :=
+begin
+  intro hem,
+  apply dn_of_peirce,
+  exact peirce_of_em hem,
+end
+
+lemma peirce_of_dn :
+  double_negation → peirce :=
+begin
+  intro hdn,
+  apply peirce_of_em,
+  exact em_of_dn hdn,
+end
 
 end backward_proofs
 
