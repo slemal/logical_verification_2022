f4a56f379db6b26b9d0255bcb0215acbee0a4751
Simon <simon.lemal@pm.me>
2023-02-04 17:13:02 +0100

    Solved ex 8

--- lean/love08_operational_semantics_exercise_sheet.lean
+++ lean/love08_operational_semantics_exercise_sheet.lean
@@ -53,14 +53,23 @@ around for a Lean bug. Below we will take `σ := state`.
 specification of GCL above. -/
 
 inductive big_step : (stmt state × state) → state → Prop
--- enter the missing `assign` rule here
+| assign {x a s} :
+  big_step (stmt.assign x a, s) (s{x ↦ a s})
 | assert {b : state → Prop} {s} (hcond : b s) :
   big_step (stmt.assert b, s) s
--- enter the missing `seq` rule here
+| seq {S T s t u}
+    (hS : big_step (S, s) t)
+    (hT : big_step (T, t) u) :
+  big_step (S ;; T, s) u
 | choice {Ss s t} (i) (hless : i < list.length Ss)
     (hbody : big_step (list.nth_le Ss i hless, s) t) :
   big_step (stmt.choice Ss, s) t
--- enter the missing `loop` rules here
+| no_loop {S s} :
+  big_step (stmt.loop S, s) s
+| loop {S s t u}
+    (hbody: big_step (S, s) t)
+    (hrest : big_step (stmt.loop S, t) u) :
+  big_step (stmt.loop S, s) u
 
 infix ` ⟹ ` : 110 := big_step
 
@@ -69,26 +78,72 @@ WHILE language. -/
 
 @[simp] lemma big_step_assign_iff {x a s t} :
   (stmt.assign x a, s) ⟹ t ↔ t = s{x ↦ a s} :=
-sorry
+begin
+  apply iff.intro; intro h,
+  { cases' h,
+    refl, },
+  { rw h,
+    exact big_step.assign, }
+end
 
 @[simp] lemma big_step_assert {b s t} :
   (stmt.assert b, s) ⟹ t ↔ t = s ∧ b s :=
-sorry
+begin
+  apply iff.intro; intro h,
+  { cases' h,
+    apply and.intro,
+    { refl, },
+    { exact hcond, } },
+  { cases' h,
+    rw left,
+    exact big_step.assert right, }
+end
 
 @[simp] lemma big_step_seq_iff {S₁ S₂ s t} :
   (stmt.seq S₁ S₂, s) ⟹ t ↔ (∃u, (S₁, s) ⟹ u ∧ (S₂, u) ⟹ t) :=
-sorry
+begin
+  apply iff.intro; intro h,
+  { cases' h,
+    apply exists.intro,
+    apply and.intro; assumption, },
+  { cases' h,
+    cases' h,
+    apply big_step.seq,
+    { exact left, },
+    { exact right, } }
+end
 
 lemma big_step_loop {S s u} :
   (stmt.loop S, s) ⟹ u ↔
   (s = u ∨ (∃t, (S, s) ⟹ t ∧ (stmt.loop S, t) ⟹ u)) :=
-sorry
+begin
+  apply iff.intro; intro h,
+  { cases' h,
+    { apply or.intro_left; refl, },
+    { apply or.intro_right,
+      apply exists.intro,
+      apply and.intro; assumption, } },
+  { cases' h,
+    { rw h; exact big_step.no_loop, },
+    { cases' h with t,
+      cases' h,
+      apply big_step.loop; assumption, } }
+end
 
 @[simp] lemma big_step_choice {Ss s t} :
   (stmt.choice Ss, s) ⟹ t ↔
   (∃(i : ℕ) (hless : i < list.length Ss),
      (list.nth_le Ss i hless, s) ⟹ t) :=
-sorry
+begin
+  apply iff.intro; intro h,
+  { cases' h,
+    apply exists.intro i,
+    apply exists.intro hless,
+    exact h, },
+  { cases' h with i,
+    cases' h with hless,
+    apply big_step.choice; assumption, }
+end
 
 end gcl
 
@@ -97,21 +152,28 @@ program, by filling in the `sorry` placeholders below. -/
 
 def gcl_of : stmt → gcl.stmt state
 | stmt.skip         := gcl.stmt.assert (λ_, true)
-| (stmt.assign x a) :=
-  sorry
-| (S ;; T)          :=
-  sorry
+| (stmt.assign x a) := gcl.stmt.assign x a
+| (S ;; T)          := (gcl_of S ;; gcl_of T)
 | (stmt.ite b S T)  :=
-  sorry
+  gcl.stmt.choice [
+    gcl.assert b ;; gcl_of S,
+    gcl.assert (λs, ¬ b s) ;; gcl_of T]
 | (stmt.while b S)  :=
-  sorry
+  gcl.stmt.loop (
+    gcl.assert b ;;
+    gcl_of S) ;;
+  gcl.stmt.assert (λs, ¬ b s)
 
 /-! 1.4. In the definition of `gcl_of` above, `skip` is translated to
 `assert (λ_, true)`. Looking at the big-step semantics of both constructs, we
 can convince ourselves that it makes sense. Can you think of other correct ways
 to define the `skip` case? -/
 
--- enter your answer here
+/-
+
+* `loop (assert _)`
+
+-/
 
 
 /-! ## Question 2: Program Equivalence
@@ -150,19 +212,53 @@ show (S₁, s) ⟹ t ↔ (S₃, s) ⟹ t, from
 
 lemma big_step_equiv.skip_assign_id {x} :
   stmt.assign x (λs, s x) ≈ stmt.skip :=
-sorry
+begin
+  intros s t; apply iff.intro; intro h,
+  { cases' h,
+    simp, },
+  { cases' h,
+    simp, }
+end
 
 lemma big_step_equiv.seq_skip_left {S : stmt} :
   stmt.skip ;; S ≈ S :=
-sorry
+begin
+  intros s t; apply iff.intro; intro h,
+  { cases' h,
+    cases' h_1,
+    exact h, },
+  { apply big_step.seq,
+    { exact big_step.skip, },
+    { exact h, } }
+end
 
 lemma big_step_equiv.seq_skip_right {S : stmt} :
   S ;; stmt.skip ≈ S :=
-sorry
+begin
+  intros s t; apply iff.intro; intro h,
+  { cases' h,
+    cases' h_1,
+    exact h, },
+  { apply big_step.seq,
+    { exact h, },
+    { exact big_step.skip, } }
+end
 
 lemma big_step_equiv.ite_seq_while {b} {S : stmt} :
   stmt.ite b (S ;; stmt.while b S) stmt.skip ≈ stmt.while b S :=
-sorry
+begin
+  intros s t; apply iff.intro; intro h,
+  { cases' h; cases' h,
+    { apply big_step.while_true; assumption, },
+    { apply big_step.while_false; assumption, } },
+  { cases' h,
+    { apply big_step.ite_true,
+      { exact hcond, },
+      { apply big_step.seq; assumption, } },
+    { apply big_step.ite_false,
+      { exact hcond, },
+      { exact big_step.skip, } } }
+end
 
 /-! 2.2 (**optional**). Program equivalence can be used to replace subprograms
 by other subprograms with the same semantics. Prove the following so-called
@@ -171,15 +267,55 @@ congruence rules: -/
 lemma big_step_equiv.seq_congr {S₁ S₂ T₁ T₂ : stmt} (hS : S₁ ≈ S₂)
     (hT : T₁ ≈ T₂) :
   S₁ ;; T₁ ≈ S₂ ;; T₂ :=
-sorry
+begin
+  intros s t; apply iff.intro; intro h,
+  { cases' h,
+    apply big_step.seq,
+    { rw ←hS; assumption, },
+    { rw ←hT; assumption, } },
+  { cases' h,
+    apply big_step.seq,
+    { rw hS; assumption, },
+    { rw hT; assumption, } }
+end
 
 lemma big_step_equiv.ite_congr {b} {S₁ S₂ T₁ T₂ : stmt} (hS : S₁ ≈ S₂)
     (hT : T₁ ≈ T₂) :
   stmt.ite b S₁ T₁ ≈ stmt.ite b S₂ T₂ :=
-sorry
+begin
+  intros s t; apply iff.intro; intro h,
+  { cases' h,
+    { apply big_step.ite_true,
+      { exact hcond, },
+      { rw ←hS; assumption, } },
+    { apply big_step.ite_false,
+      { exact hcond, },
+      { rw ←hT; assumption, } } },
+  { cases' h,
+    { apply big_step.ite_true,
+      { exact hcond, },
+      { rw hS; assumption, } },
+    { apply big_step.ite_false,
+      { exact hcond, },
+      { rw hT; assumption, } } }
+end
 
 lemma denote_equiv.while_congr {b} {S₁ S₂ : stmt} (hS : S₁ ≈ S₂) :
   stmt.while b S₁ ≈ stmt.while b S₂ :=
-sorry
+begin
+  intros s t; apply iff.intro; intro h,
+  { induction' h,
+    { apply big_step.while_true,
+      { exact hcond, },
+      { rw ←hS; assumption, },
+      { apply ih_h_1; assumption, } },
+    { apply big_step.while_false; assumption, } },
+  { induction' h,
+    { apply big_step.while_true,
+      { exact hcond, },
+      { rw hS; assumption, },
+      { apply ih_h_1; assumption, } },
+    { apply big_step.while_false; assumption, } }
+end
 
 end LoVe
